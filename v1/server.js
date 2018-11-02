7bef70cd43a879d2ae020809d8d07078b6ad9b11
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-11-02 15:35:41 -0700

    server and database test connection

--- v1/server.js
+++ v1/server.js
@@ -2,4 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const pgp = require('pg-promise')(/*options*/)
+const db = pgp('postgres://sierrakilo@localhost:5432/sampledb')
+
+db.many('SELECT name FROM people;', 123)
+  .then((data) => {
+    data.forEach(name => {
+      console.log(`Name: ${name.name}`)
+    })
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
