97db11abaef5107636ee02bc25922aca52567764
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-11-24 21:40:25 -0800

    added add item form, item list to front end

--- v3/src/client/App.js
+++ v3/src/client/App.js
@@ -7,26 +7,42 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allCheckLists: [],
+      allChecklists: [],
+      allItems: []
     }
 
-  this.fetchCheckLists = this.fetchCheckLists.bind(this)
+  this.fetchChecklists = this.fetchChecklists.bind(this)
   this.postChecklist = this.postChecklist.bind(this)
   this.deleteChecklist = this.deleteChecklist.bind(this)
+  this.fetchItems = this.fetchItems.bind(this)
+  // this.postItem = this.postItem.bind(this)
+  // this.deleteItem = this.deleteItem.bind(this)
 
   }
 
   componentDidMount() {
-    this.fetchCheckLists()
+    this.fetchChecklists()
+    this.fetchItems()
   }
 
-  fetchCheckLists = () => {
+  fetchChecklists = () => {
     fetch("/api/checklist")
       .then(res => res.json())
       .then(parsedJSON => parsedJSON.map(checklists => ({
         checklist: `${checklists.name}`,
         id: `${checklists.checklist_id}`
-      }))).then(checklists => this.setState({allCheckLists: checklists}))
+      })))
+      .then(checklists => this.setState({allChecklists: checklists}))
+  }
+
+  fetchItems = () => {
+    fetch('/api/item')
+      .then(res => res.json())
+      .then(parsedJSON => parsedJSON.map(item => ({
+        item: `${item.name}`,
+        id: `${item.item_id}`
+      })))
+      .then(items => this.setState({allItems: items}))
   }
 
   postChecklist = (e) => {
@@ -42,10 +58,26 @@ export default class App extends Component {
     })
     .then(location.reload())
     .then(res => res.json())
-      .then(response => console.log('Success:', JSON.stringify(response)))
-      .catch(err => console.error('Error:', err));
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(err => console.error('Error:', err));
   }
 
+  postItem = (e) => {
+    fetch('/api/item', {
+      method: 'POST',
+      body: JSON.stringify(
+        {
+          'name': document.getElementById('itemName').value,
+          'description': document.getElementById('itemDescription').value}), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(location.reload())
+    .then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(err => console.error('Error:', err));
+  }
 
   deleteChecklist = (checklist_id) => {
     fetch('/api/checklist', {
@@ -60,49 +92,105 @@ export default class App extends Component {
     })
     .then(location.reload())
     .then(res => res.json())
-      .then(response => console.log('Success:', JSON.stringify(response)))
-      .catch(err => console.error('Error:', err));
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(err => console.error('Error:', err));
+  }
+
+  deleteItem = (item_id) => {
+    fetch('/api/item', {
+      method: 'DELETE',
+      body: JSON.stringify(
+        {
+          'id': item_id,
+        }), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(location.reload())
+    .then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(err => console.error('Error:', err));
   }
 
   render() {
     return (
       <div className='list-view-collection'>
         <div>
-        <h2>Add Checklist</h2>
-        <div>
-          <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            this.postChecklist()
-          }}
-          >
-            <input id='name' type='text' placeholder='name'></input>
-            <input id='description' type='text'  placeholder='description'></input>
-            <button type='submit'>Submit</button>
-          </form>
-        </div>
+          <h2>Add Checklist</h2>
+          <div>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                this.postChecklist()
+              }}
+            >
+              <input id='name' type='text' placeholder='name'></input>
+              <input id='description' type='text'  placeholder='description'></input>
+              <button type='submit'>Submit</button>
+            </form>
+          </div>
         </div>
         <div>
           <h2>Checklists</h2>
           <div>
-            <table className='collection'>
+            <table className='collection' style={{margin: "0px auto"}}>
               <tbody>
-               {this.state.allCheckLists.map((checklist) => {
-                 return (
-                   <tr>
-                   <td>{ checklist.checklist }  </td>
-                   <td className='id'>{ checklist.id }  </td>
-                   <td><button onClick={e => {
-                     e.preventDefault()
-                     this.deleteChecklist(checklist.id)}}
-                   >Delete</button></td>
-
+                {this.state.allChecklists.map((checklist) => {
+                  return (
+                    <tr>
+                      <td>{ checklist.checklist }  </td>
+                      <td className='id'>{ checklist.id }  </td>
+                      <td>
+                        <button onClick={e => {
+                          e.preventDefault()
+                          this.deleteChecklist(checklist.id)}}
+                        >
+                        Delete</button>
+                      </td>
                    </tr>
-                 )
-                 })}
+                 )})}
               </tbody>
             </table>
-
+          </div>
+        </div>
+        <hr></hr>
+        <div>
+          <h2>Add Item</h2>
+          <div>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                this.postItem()
+              }}
+            >
+              <input id='itemName' type='text' placeholder='name'></input>
+              <input id='itemDescription' type='text'  placeholder='description'></input>
+              <button type='submit'>Submit</button>
+            </form>
+          </div>
+          <div>
+            <h2>Item List</h2>
+            <div>
+              <table className='collection' style={{margin: "0px auto"}}>
+                <tbody>
+                  {this.state.allItems.map((item) => {
+                    return (
+                      <tr>
+                        <td>{ item.item }  </td>
+                        <td className='id'>{ item.id }  </td>
+                        <td>
+                          <button onClick={e => {
+                            e.preventDefault()
+                            this.deleteItem(item.id)}}
+                          >
+                          Delete</button>
+                        </td>
+                     </tr>
+                   )})}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
