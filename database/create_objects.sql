db6f8a6a7885b74a6ee85762d5463a6c697dd79b
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-12-08 14:16:28 -0800

    commented out part of objects, and manually removed them from the database

--- database/create_objects.sql
+++ database/create_objects.sql
@@ -20,23 +20,6 @@ CREATE TABLE IF NOT EXISTS checklist_item (
   PRIMARY KEY (checklist_id, item_id)
 );
 
-CREATE TABLE IF NOT EXISTS checklist_due (
-  id SERIAL NOT NULL UNIQUE,
-  checklist_id INT NOT NULL,
-  date_due DATE NOT NULL,
-  time_due TIME NOT NULL,
-  FOREIGN KEY (checklist_id) REFERENCES checklist(id),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS submitted_checklist (
-  id SERIAL NOT NULL UNIQUE,
-  checklist_id INT NOT NULL,
-  time_submitted TIMESTAMP NOT NULL,
-  FOREIGN KEY (checklist_id) REFERENCES checklist(id),
-  PRIMARY KEY (id)
-);
-
 CREATE TYPE response_type AS ENUM('YES', 'NO', 'N/A');
 
 CREATE TABLE IF NOT EXISTS response (
@@ -45,13 +28,31 @@ CREATE TABLE IF NOT EXISTS response (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS submitted_item (
-  id SERIAL NOT NULL UNIQUE,
-  submitted_checklist_id INT NOT NULL,
-  item_id INT NOT NULL,
-  response_id INT NOT NULL,
-  FOREIGN KEY (submitted_checklist_id) REFERENCES submitted_checklist(id),
-  FOREIGN KEY (item_id) REFERENCES item(id),
-  FOREIGN KEY (response_id) REFERENCES response(id),
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS submitted_item (
+--   id SERIAL NOT NULL UNIQUE,
+--   submitted_checklist_id INT NOT NULL,
+--   item_id INT NOT NULL,
+--   response_id INT NOT NULL,
+--   FOREIGN KEY (submitted_checklist_id) REFERENCES submitted_checklist(id),
+--   FOREIGN KEY (item_id) REFERENCES item(id),
+--   FOREIGN KEY (response_id) REFERENCES response(id),
+--   PRIMARY KEY (id)
+-- );
+
+
+-- CREATE TABLE IF NOT EXISTS checklist_due (
+--   id SERIAL NOT NULL UNIQUE,
+--   checklist_id INT NOT NULL,
+--   date_due DATE NOT NULL,
+--   time_due TIME NOT NULL,
+--   FOREIGN KEY (checklist_id) REFERENCES checklist(id),
+--   PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS submitted_checklist (
+--   id SERIAL NOT NULL UNIQUE,
+--   checklist_id INT NOT NULL,
+--   time_submitted TIMESTAMP NOT NULL,
+--   FOREIGN KEY (checklist_id) REFERENCES checklist(id),
+--   PRIMARY KEY (id)
+-- );
