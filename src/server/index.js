2c9a668bca30058a85aa1a810359ad97f366b371
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-12-14 21:41:19 -0800

    changed PORT to port

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 3001
+const port = process.env.PORT || 3001
 
 app.use(bodyParser.json())
 app.use(
@@ -18,4 +18,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(dist, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`)).on('error', function(err) { console.log(err)})
+app.listen(port, () => console.log(`Listening on port ${port}`)).on('error', function(err) { console.log(err)})
